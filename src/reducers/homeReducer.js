c8eb763a28df9796b6a8e8202717b3378392eaf0
prateek singh <prateek3693@gmail.com>
2022-05-08 15:10:33 +0530

    update build folder

--- src/reducers/homeReducer.js
+++ src/reducers/homeReducer.js
@@ -1,22 +1,19 @@
-import {SAVE_NUMBER_PLAYER} from '../constants'
+import { SAVE_NUMBER_PLAYER } from "../constants";
 
 const initialState = {
-    numberOfPlayer: ''
+  numberOfPlayer: "",
 };
 
-
 const home = (state = initialState, action) => {
-    switch (action.type) {
-      case SAVE_NUMBER_PLAYER:
-          console.log(action.payload)
-        return {
-            ...state,
-            numberOfPlayer: action.payload
-        }
-      default:
-        return state
-    }
+  switch (action.type) {
+    case SAVE_NUMBER_PLAYER:
+      return {
+        ...state,
+        numberOfPlayer: action.payload,
+      };
+    default:
+      return state;
   }
-  
-  export default home;
-  
+};
+
+export default home;
